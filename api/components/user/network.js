9cfc423467418c41754e5af605ece22b87da4b11
infosergio2020 <infosergio2020@gmail.com>
2024-08-14 15:03:09 -0300

    se modifican los catch de network de user para derivar los errores al middleware del index

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -12,20 +12,21 @@ router.put('/', secure('update'),upsert);
 router.delete('/:id', remove);
 
 //Internal functions
+
 /**
  * Controlador para obtener la lista de usuarios.
  * 
  * @param {express.Request} req - Objeto de solicitud de Express.
  * @param {express.Response} res - Objeto de respuesta de Express.
  */
-function list(req, res) {
+function list(req, res, next) {
     Controller.list()
         .then((lista) => {
             response.success(req, res, lista, 200);
-        })
-        .catch((err) => {
-            response.error(req, res, err, err.message, 500);
-        });
+        }).catch(next);
+        // .catch((err) => {
+        //     response.error(req, res, err, err.message, 500);
+        // });
 }
 
 /**
@@ -34,14 +35,14 @@ function list(req, res) {
  * @param {express.Request} req - Objeto de solicitud de Express.
  * @param {express.Response} res - Objeto de respuesta de Express.
  */
-function get(req, res) {
+function get(req, res, next) {
     Controller.get(req.params.id)
         .then((user) => {
             response.success(req, res, user, 200);
-        })
-        .catch((err) => {
-            response.error(req, res, err, err.message, 500);
-        });
+        }).catch(next);
+        // .catch((err) => {
+        //     response.error(req, res, err, err.message, 500);
+        // });
 }
 
 /**
@@ -50,14 +51,14 @@ function get(req, res) {
  * @param {express.Request} req - Objeto de solicitud de Express.
  * @param {express.Response} res - Objeto de respuesta de Express.
  */
-function upsert(req, res) {
+function upsert(req, res, next) {
     Controller.upsert(req.body)
         .then((user) => {
             response.success(req, res, user, 200); // Aquí se usa `req.body` porque contiene los datos del usuario.
-        })
-        .catch((err) => {
-            response.error(req, res, err.message, 500);
-        });
+        }).catch(next);
+        // .catch((err) => {
+        //     response.error(req, res, err.message, 500);
+        // });
 }
 
 /**
@@ -66,7 +67,7 @@ function upsert(req, res) {
  * @param {express.Request} req - Objeto de solicitud de Express.
  * @param {express.Response} res - Objeto de respuesta de Express.
  */
-function remove(req, res) {
+function remove(req, res, next) {
     const userId = req.params.id;
 
     Controller.remove(userId)
@@ -76,10 +77,10 @@ function remove(req, res) {
             } else {
                 response.error(req, res, "User not found", 404);
             }
-        })
-        .catch((err) => {
-            response.error(req, res, err.message, 500);
-        });
+        }).catch(next);
+        // .catch((err) => {
+        //     response.error(req, res, err.message, 500);
+        // });
 }
 
 module.exports = router;
