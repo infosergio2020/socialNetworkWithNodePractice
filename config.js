58063f23e523635505dbb1169a02738c85a69390
infosergio2020 <infosergio2020@gmail.com>
2024-08-21 14:37:31 -0300

    se agrega parametros de configuracion de sql

--- config.js
+++ config.js
@@ -5,4 +5,11 @@ module.exports = {
     jwt:{
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
+    mysql:{
+        host: process.env.MYSQL_HOST || '192.168.0.102',
+        user: process.env.MYSQL_USER || 'admin01',
+        password: process.env.MYSQL_PASS || 'admin123',
+        database: process.env.MYSQL_DB || 'social_network',
+        port: process.env.MYSQL_PORT || '3306',
+    }
 }
