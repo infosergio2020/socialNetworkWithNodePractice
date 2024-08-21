64b31300412cd6d475aae2cae768528ee5e9c053
infosergio2020 <infosergio2020@gmail.com>
2024-08-21 14:41:47 -0300

    se agrega importacion de controlador mysql a index de user y se comenta controlador dummy

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,4 +1,5 @@
-const store = require('./../../../store/dummy');
+//const store = require('./../../../store/dummy');
+const store = require('./../../../store/mysql');
 const controller = require('./controller');
 
 // injectamos el store al controlador para que store no quede aislado dentro del propio archivo
